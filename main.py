5cb2bcfd7a3e44108c38e782c20f4cb4978524d7
Dark99965 <noah.retter@icloud.com>
2026-04-27 15:00:53 -0300

    added muitable routes and 404

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "<h1>Hello, world</h1>"
+
+
+@app.route('/test/<name>')
+def test(name):
+    return render_template("index.html", person=name)
+
+@app.route('/test')
+def test1():
+    return render_template("testerror.html")
+
+@app.route("/<url>")
+def error(url):
+    return render_template("404.html", link=url)
